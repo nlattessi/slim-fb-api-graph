2467656cbf859ce13ddc79066532ad417692d084
Nahuel Lattessi <n_lattessi@hotmail.com>
2017-10-19 14:00:53 -0300

    boostraping project structure and add useless endpoint for testing project so far

---
+++ app/routes.php
@@ -0,0 +1,11 @@
+<?php
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
