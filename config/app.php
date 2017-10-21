166842db18dc78989ca3513578715e91b1afdc80
Nahuel Lattessi <n_lattessi@hotmail.com>
2017-10-21 20:37:21 -0300

    remove app_id and app_secret

--- config/app.php
+++ config/app.php
@@ -7,8 +7,8 @@ return [
 
         // Facebook PHP SDK Settings
         'facebook' => [
-            'app_id'                => '1662204533855030',
-            'app_secret'            => '05dc613a98d081364c4cb13402b6811a',
+            'app_id'                => '',
+            'app_secret'            => '',
             'default_graph_version' => 'v2.10',
             'defaultFields'         => ['id', 'first_name', 'last_name'],
         ],
